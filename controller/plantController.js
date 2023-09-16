92e49c0f426fd8a549978c3311f1cbfdea4c0940
Vivek Pathak <vivekpathak2083@gmail.com>
2023-09-17 01:13:45 +0530

    controllers added

--- controller/plantController.js
+++ controller/plantController.js
@@ -1,6 +1,7 @@
 import plantModel from "../models/plantModel.js";
 import fs from "fs";
 import csv from "csvtojson";
+import feedbackModel from "../models/feedbackModel.js";
 
 export const getPlantController = async (req, res) => {
   try {
@@ -54,16 +55,47 @@ export const uploadPlantController = async (req, res) => {
 };
 export const postFeedback = async (req, res) => {
   try {
+    const { score, description } = req.body; //req.fileds
+    if (!score) {
+      return res.send({ message: "Name is required!" });
+    }
+    if (!description) {
+      return res.send({ message: "Speciality is required!" });
+    }
+    const feed = await new feedbackModel({
+      score,
+      description,
+    }).save();
     res.status(200).send({
       success: true,
-      message: "upload successful",
+      message: "review posted successfully",
+      feed,
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
       error,
-      message: "error in getting plant details",
+      message: "error in posting review",
+    });
+  }
+};
+
+export const getFeedback = async (req, res) => {
+  try {
+    const feed = await feedbackModel.find({}).sort({ createdAt: -1 });
+    res.status(201).send({
+      success: true,
+      totalCount: feed.length,
+      message: "Feedback",
+      feed,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "error in getting feedback",
     });
   }
 };

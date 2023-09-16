d9e1df41515c0cdf60ec05c56761ddac471bde2b
Vivek Pathak <vivekpathak2083@gmail.com>
2023-09-16 21:29:54 +0530

    controllers for get and post of plant objects defined

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import colors from "colors";
+import connectDB from "./config/db.js";
 
 import cors from "cors";
 

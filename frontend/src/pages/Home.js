6b9f44e4e57818f74f3e6a3f72faaf1a7663e252
Anushka Antra <antraanushka31@gmail.com>
2023-09-17 02:28:32 +0530

    updated the home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,27 @@
 import React from "react";
-// import title from "../Assest/title_background";
-import title from "../Assest/Titile_background.jpg";
+import logo from "../Assets/Logo.png";
 
 const Home = () => {
   return (
     <div className="Home">
       <div className="content">
-        <img
-          src={title}
-          alt="title_background"
-          height={{ width: "fit-content", height: "100" }}
-        />
+        {/* <img src={title} alt="title_background" /> */}
+        <div className="title">
+            <h1>Medicinal Plant Detection: The Future of Herbal Medicine</h1>
+        </div>
         <div className="para">
-          <p>
-            The Website aims to develop a machine learning model for the
-            identification and classification of medicinal plants from images
-            and create a user-friendly application to provide valuable
-            information about the plants.
-          </p>
+            <p>
+                The Website aims to develop a machine learning model for the
+                identification and classification of medicinal plants from images
+                and create a user-friendly application to provide valuable
+                information about the plants.
+            </p>
+        </div>
+        <div className="foot">
+            <button type="button" className="btn btn-success" style={{ height: 50 }}>
+                Try It
+            </button>
+            <img src={logo} alt="Logo" style={{ width: 200, height: 200 }} />
         </div>
       </div>
     </div>

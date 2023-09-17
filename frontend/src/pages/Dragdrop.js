3fa1e097b11693ab29e59a88e4e5c86b270ce543
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2023-09-17 07:52:54 +0530

    resolved conflict in dragdrop.js

--- frontend/src/pages/Dragdrop.js
+++ frontend/src/pages/Dragdrop.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import Footer from "./Footer.js";
-import { useState } from "react";
-import { useDropzone } from "react-dropzone";
-import "../style/dragDropStyle.css";
-=======
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import upload from "../Assets/upload_image.png";
 import "../style/dragDropStyle.css";
 import Footer from "./footer";
->>>>>>> upstream/Anushka
 
 const thumbsContainer = {
   display: "flex",
@@ -50,7 +43,6 @@ const container = {
 
 const box = {
   border: "1px solid lightblue",
-
 };
 
 function Dragdrop(props) {
@@ -91,13 +83,15 @@ function Dragdrop(props) {
       <section className="container" style={container}>
         <div {...getRootProps({ className: "dropzone" })}>
           <input {...getInputProps()} />
-          <img src={upload} alt="upload" style={{width: 60}}/>
+          <img src={upload} alt="upload" style={{ width: 60 }} />
           <h2>
             Click here to add files <br />
             or
             <br /> Drag & Drop files here
           </h2>
-          <button class="btn btn-outline-info" style={{marginTop: 0}}>Browse Files</button>
+          <button class="btn btn-outline-info" style={{ marginTop: 0 }}>
+            Browse Files
+          </button>
         </div>
         <aside style={thumbsContainer}>{thumbs}</aside>
       </section>

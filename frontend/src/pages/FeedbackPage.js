85f79ce3bd7c81c53c0bd73da8bb905655de2cc1
Vivek Pathak <vivekpathak2083@gmail.com>
2023-09-17 14:50:11 +0530

    Logo and feedback updated

---
+++ frontend/src/pages/FeedbackPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Feedback from "./Feedback";
+import Footer from "./footer";
+
+const FeedbackPage = () => {
+  return (
+    <>
+      <Feedback />
+      <Footer />
+    </>
+  );
+};
+
+export default FeedbackPage;
